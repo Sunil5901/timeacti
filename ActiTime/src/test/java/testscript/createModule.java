62efac9a86202d479009f10dbffeb35968fd867c
SUNIL <SUNIL@LAPTOP-L8IHVVBH>
2023-01-16 22:18:09 +0530

    actitime

---
+++ ActiTime/src/test/java/testscript/createModule.java
@@ -0,0 +1,12 @@
+package testscript;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+@Test
+public class createModule {
+public void creatCustomer() {
+	Reporter.log("creatCustomer",true);
+}
+
+}
